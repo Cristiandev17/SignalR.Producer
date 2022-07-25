6e37a632fcf268fcbea6af99f32c326c4334fe9e
cristian_cam17 <cristian.aristizabal@intelligisgroup.com>
2022-07-25 11:26:20 -0500

    generador de mensajes para SignalR

---
+++ MessageProducer/Main.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace MessageProducer;
+public class Main
+{
+    HubConnection connection;
+    public Main()
+    {
+
+        connection = new HubConnectionBuilder()
+            .WithUrl("https://localhost:7252/TipHub")
+            .WithAutomaticReconnect()
+            .Build();
+
+        connection.Closed += async (error) =>
+        {
+            await Task.Delay(new Random().Next(0, 5) * 1000);
+            await connection.StartAsync();
+        };
+    }
+
+    public async void Connect()
+    {
+        try
+        {
+            await connection.StartAsync();
+
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+
+    public async void SendMessage(string message)
+    {
+        try
+        {
+            await connection.InvokeAsync("SendMessage", "Camilo", message);
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+}
---
+++ MessageProducer/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MessageProducer;
+
+var key = "1";
+var message = "";
+
+Console.WriteLine("Press any key to connect...");
+Console.ReadLine();
+
+Main main = new Main();
+main.Connect();
+
+while (key == "1")
+{
+    if (!string.IsNullOrEmpty(message))
+    {
+        main.SendMessage(message);
+    }
+
+    Console.WriteLine("Choose an option from the following list:");
+    Console.WriteLine("\t1 - New Message");
+    Console.WriteLine("\t2 - Close app");
+    Console.Write("Your option? ");
+    key = Console.ReadLine();
+
+    if (key == "1")
+    {
+        Console.WriteLine("Write a new message");
+        message = Console.ReadLine();
+    }
+}
